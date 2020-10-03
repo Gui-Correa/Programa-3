0130266b29ee52245d4a5f8e8d4eec49a94c7e81
Gui-Correa <71907354+Gui-Correa@users.noreply.github.com>
2020-10-02 21:20:43 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercico_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double valor, resultado;
+            Console.WriteLine("Digite um valor inteiro: ");
+            valor = Convert.ToInt32(Console.ReadLine());
+            resultado = Convert.ToInt32(Math.Sqrt(valor));
+
+            Console.WriteLine("Resultado: " + resultado);
+
+            Console.ReadKey();
+               
+        }
+    }
+}
